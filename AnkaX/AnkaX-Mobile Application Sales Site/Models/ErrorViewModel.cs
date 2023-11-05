48370bbcec7b75ad63e0653a11fb2f84b0779207
erhancekin <ummugulsumerdim@gmail.com>
2023-11-05 14:00:51 +0300

    MVC Project Created

---
+++ AnkaX/AnkaX-Mobile Application Sales Site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnkaX_Mobile_Application_Sales_Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
