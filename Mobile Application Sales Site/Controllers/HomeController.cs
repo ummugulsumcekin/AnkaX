dc12b8d663f00d7a5eff77596f4751572fddb1af
erhancekin <ummugulsumerdim@gmail.com>
2023-11-08 22:42:55 +0300

    update

--- Mobile Application Sales Site/Controllers/HomeController.cs
+++ Mobile Application Sales Site/Controllers/HomeController.cs
@@ -28,5 +28,6 @@ namespace Mobile_Application_Sales_Site.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
