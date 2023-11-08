2b1fb3d2099a5868d3454bc6e83819fb83e5cb0b
Ummugulsum Cekin <102469765+ummugulsumcekin@users.noreply.github.com>
2023-11-08 16:24:20 +0300

    Delete AnkaX/AnkaX-Mobile Application Sales Site directory

--- AnkaX/AnkaX-Mobile Application Sales Site/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AnkaX_Mobile_Application_Sales_Site.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
