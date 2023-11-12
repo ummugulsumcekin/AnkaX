ae854ca1288d1903975c0a0793ae9615cbbd8091
erhancekin <ummugulsumerdim@gmail.com>
2023-11-12 21:11:51 +0300

    before create  database

---
+++ Mobile Application Sales Site/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Mobile_Application_Sales_Site.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+    }
+}
---
+++ Mobile Application Sales Site/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Mobile_Application_Sales_Site.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
