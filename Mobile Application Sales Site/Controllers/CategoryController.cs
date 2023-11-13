351659ada034c85010a14b6d8639b70d76f58d43
erhancekin <ummugulsumerdim@gmail.com>
2023-11-13 23:44:58 +0300

    seed category table

---
+++ Mobile Application Sales Site/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mobile_Application_Sales_Site.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
