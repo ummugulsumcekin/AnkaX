2b7b324d7e5fbacfb7fd0628e6e90718affe25bc
erhancekin <ummugulsumerdim@gmail.com>
2023-11-14 20:51:45 +0300

    display all categories

--- Mobile Application Sales Site/Controllers/CategoryController.cs
+++ Mobile Application Sales Site/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Mobile_Application_Sales_Site.Data;
+using Mobile_Application_Sales_Site.Models;
 
 namespace Mobile_Application_Sales_Site.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+         public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
