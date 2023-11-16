cd28cf7ebd15ad16ec15ccd09a596720ea802567
erhancekin <ummugulsumerdim@gmail.com>
2023-11-16 15:21:48 +0300

    edit&update category

--- Mobile Application Sales Site/Controllers/CategoryController.cs
+++ Mobile Application Sales Site/Controllers/CategoryController.cs
@@ -7,7 +7,7 @@ namespace Mobile_Application_Sales_Site.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
-         public CategoryController(ApplicationDbContext db)
+        public CategoryController(ApplicationDbContext db)
         {
             _db = db;
 
@@ -26,10 +26,10 @@ namespace Mobile_Application_Sales_Site.Controllers
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
-              ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
-             }
-            
-           
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
@@ -37,7 +37,39 @@ namespace Mobile_Application_Sales_Site.Controllers
                 return RedirectToAction("Index");
             }
             return View();
-           
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id==0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+           // Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            if (categoryFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
+
+   
