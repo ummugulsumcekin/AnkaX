b7c507d0e7d1c5a7c9e2af2df8d69e810f893f75
erhancekin <ummugulsumerdim@gmail.com>
2023-11-21 20:17:03 +0300

    view models in action

--- Mobile Application Sales Site/Areas/Admin/Controllers/ProductController.cs
+++ Mobile Application Sales Site/Areas/Admin/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using AnkaX.Models;
 using AnkaX.DataAccess.Repository.IRepository;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AnkaX.Models.ViewModels;
 
 namespace Mobile_Application_Sales_Site.Areas.Admin.Controllers
 {
@@ -19,22 +21,33 @@ namespace Mobile_Application_Sales_Site.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            
             return View(objProductList);
         }
 
         public IActionResult Create()
-        {
-            return View();
+        { 
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().
+                Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+
+                }),
+            Product = new Product()
+            };
+            return View(productVM);
         }
 
         [HttpPost]
-        public IActionResult Create(Product obj)
-        {
-           
+        public IActionResult Create(ProductVM obj)
+        { 
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(obj.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
