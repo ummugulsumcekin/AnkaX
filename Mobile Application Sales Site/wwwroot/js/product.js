797146e9b52440a39e10cb995d637b74c2891a47
erhancekin <ummugulsumerdim@gmail.com>
2023-11-22 14:46:18 +0300

    load datatables

---
+++ Mobile Application Sales Site/wwwroot/js/product.js
@@ -0,0 +1,23 @@
+﻿
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url:'/admin/product/getall'},
+    
+       "columns": [
+           { data: 'title', "width": "15%" }, 
+           { data: 'siteAge', "width": "15%" }, 
+           { data: 'industry', "width": "15%" },
+           { data: 'location', "width": "15%" },
+           { data: 'monthlyNetProfit', "width": "15%" },
+           { data: 'category.name', "width": "15%" }
+           
+           
+    ]
+    });
+}
+
+   
