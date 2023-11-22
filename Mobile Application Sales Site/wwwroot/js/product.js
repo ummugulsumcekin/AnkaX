57c2af8a4b8c097fef3bd98d483fb8f83f44cf00
erhancekin <ummugulsumerdim@gmail.com>
2023-11-22 16:21:20 +0300

    delete products and alert

--- Mobile Application Sales Site/wwwroot/js/product.js
+++ Mobile Application Sales Site/wwwroot/js/product.js
@@ -1,4 +1,4 @@
-﻿
+﻿var dataTable;
 $(document).ready(function () {
     loadDataTable();
 });
@@ -8,16 +8,47 @@ function loadDataTable() {
         "ajax": { url:'/admin/product/getall'},
     
        "columns": [
-           { data: 'title', "width": "15%" }, 
-           { data: 'siteAge', "width": "15%" }, 
-           { data: 'industry', "width": "15%" },
+           { data: 'title', "width": "28%" }, 
+           { data: 'siteAge', "width": "10%" }, 
+           { data: 'industry', "width": "12%" },
            { data: 'location', "width": "15%" },
            { data: 'monthlyNetProfit', "width": "15%" },
-           { data: 'category.name', "width": "15%" }
+           { data: 'category.name', "width": "12%" },
+           {
+               data: 'id',
+               "render": function (data) {
+                   return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+               },
+               "width": "25%"
+           }
            
            
     ]
     });
 }
-
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    })
+}
    
