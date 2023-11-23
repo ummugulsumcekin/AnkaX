b352b4d458f234a2e1250bb5100f830af3e8379b
erhancekin <ummugulsumerdim@gmail.com>
2023-11-23 15:34:54 +0300

    scaffold ıdentity ıssue

--- Mobile Application Sales Site/Program.cs
+++ Mobile Application Sales Site/Program.cs
@@ -2,6 +2,7 @@ using AnkaX.DataAccess.Data;
 using AnkaX.DataAccess.Repository;
 using AnkaX.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 
 
@@ -11,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 //builder.Services.AddDbContext<ApplicationDbContext>(options=>
 //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
