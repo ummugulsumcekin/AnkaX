615f9a943f23b63d325a4605a36a3b808681effb
erhancekin <ummugulsumerdim@gmail.com>
2023-11-29 23:30:36 +0300

    uı bug fixed

---
+++ AnkaX.DataAccess/Migrations/20231129200953_UpdateNameToBeStringApplicationUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnkaX.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNameToBeStringApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- AnkaX.Models/ApplicationUser.cs
+++ AnkaX.Models/ApplicationUser.cs
@@ -12,7 +12,7 @@ namespace AnkaX.Models
         public class ApplicationUser: IdentityUser
     {
         [Required]
-        public int Name { get; set; }
+        public string Name { get; set; }
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
